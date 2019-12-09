17cd1279fbb9a41d03f1b67a824176c4033985fe
yubing <1064085384@qq.com>
2019-12-09 21:03:42 +0800

    updated by yubing at 2019/12/09 21:03

--- batch_file_rename.py
+++ batch_file_rename.py
@@ -1,3 +1,25 @@
 # batch_file_rename
-# created by bing yu at 2019/12/09(hai nan)
+# Created by yub at 2019/12/09
+import os
 
+# define a update flag
+update_flag = 2
+
+
+def batch_rename(work_dir):
+    # os.listdir获取目录文件清单，是列表形式
+    for filename in os.listdir(work_dir):
+        print(filename)
+        if update_flag == 1:
+            new_name = 'test' + filename
+        elif update_flag == 2:
+            name = filename.replace('test','')
+            new_name = name
+        print(new_name)
+        # 定义本文件调用才能更改
+        if __name__ == '__main__':
+            os.rename(work_dir + filename, work_dir+'/'+new_name)
+
+
+file = r'C:\\Users\\10640\\Desktop\\金盘项目\\02蓝图设计\\TO-BE流程\\'
+batch_rename(file)
